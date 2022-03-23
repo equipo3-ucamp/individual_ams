cc5c031032646ef1549ba0a379558ab01d3596c2
ayax-m <ayax.morales@gmail.com>
2022-03-22 21:53:20 -0600

    Incluye comentarios PalabrasEnOrden

--- IND_S06_PalabrasEnOrden.js
+++ IND_S06_PalabrasEnOrden.js
@@ -2,8 +2,14 @@ var array = [];
 var temp;
 var len;
 
-/* palabraEnOrden(['naranja', 'manzana', 'uva', 'banana'])
-Array(4) [ "banana", "manzana", "naranja", "uva" ]
+/* El for interno va recorriendo el arreglo comparando el elemento 1 con el 2, luego el 2 con el 3, etc. 
+   esto lo hace tantas veces como lo indique la variable len. En esa comparación si el primer elemento comparado es 
+   "mayor" que el segundo elemento comparado entonces los intercambia de lugar, de otra manera no hace nada.
+
+   Con el for externo hace que se repita la operación tantas veces como elementos en el array -1 existan y en cada 
+   ciclo disminuye el valor de len porque al finalizar cada ciclo del for interno en el último lugar queda el valor 
+   más grande del array por lo que no es necesario realizar el ciclo una vez mas.
+
 */
 
 function palabraEnOrden(array) {
@@ -22,5 +28,4 @@ function palabraEnOrden(array) {
     }
     console.log(array)    
 }
-
 

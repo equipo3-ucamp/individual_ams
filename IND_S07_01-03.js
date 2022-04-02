d7cf948f080c133ad45421caf492e340213e419c
ayax-m <ayax.morales@gmail.com>
2022-04-01 20:15:08 -0600

    +busquedaObjeto

--- IND_S07_01-03.js
+++ IND_S07_01-03.js
@@ -1,20 +1,28 @@
-
-function eliminarIndice(array, indice) {    
-    if (indice < array.length && indice > -1) {
-        array.splice(indice, 1)
-    } else {
-        console.log("Indice fuera de rango");
-    }    
+function eliminarIndice(array, indice) {
+    (indice < array.length && indice > -1) ? array.splice(indice, 1) : console.log("Indice fuera de rango");
     console.log(array);
 }
 
 
+
 function busquedaArray(array, item) {
-    if (array.indexOf(item) > -1) {
-        console.log("Se encontró el elemento");
-    } else {
-        console.log("No se encontró el elemento");
-    }   
+    return (array.indexOf(item) > -1) ? console.log("Se encontró el elemento") : console.log("No se encontró el elemento");
 }
 
 
+
+let infoLibros = [
+    { titulo: "El Quijote", autor: "Miguel C."},
+    { titulo: "Señor de las moscas", autor: "William G."},
+    { titulo: "La tregua", autor: "Mario B."},
+    ];
+
+function busquedaObjeto(array, item) {
+    let encontrado = false;
+    for (let key in array) {
+        if ((array[key]["titulo"] == item) || (array[key]["autor"] == item)) {
+            encontrado = true;
+        }
+    }
+    encontrado ? console.log("Se encontró el elemento") : console.log("No se encontró el elemento");
+}
